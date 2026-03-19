daab742fb8c01ff53806bd00ddb4a1dfb2d0b5fe
viniciuszawi <viniciuszawi@gmail.com>
2026-03-19 19:17:29 -0300

    Revert "change PDF button label to 'Baixar PDF'"
    This reverts commit 56cc9574569eb5acd9f887e0e5bfff7f5fd835ad.

--- src/App.jsx
+++ src/App.jsx
@@ -1197,7 +1197,7 @@ function App() {
                                         <polyline points="7 10 12 15 17 10"></polyline>
                                         <line x1="12" y1="15" x2="12" y2="3"></line>
                                     </svg>
-                                    Baixar PDF
+                                    PDF
                                 </button>
                                 <button className="btn-close" onClick={() => setViewSettlementDetails(false)}>
                                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
